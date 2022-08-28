4f8d93fe7cbab5e741c4a71ec0e1cae778df3266
Slawek01~ <lilcrunker@gmail.com>
2022-08-28 23:35:47 +0200

    Added speed, colors and updating slider percente number

--- script.js
+++ script.js
@@ -8,43 +8,53 @@ const squares = 546
 let sliderValue = 70
 let range = 360
 
-const createSquares = () => {
+const createSquare = speed => {
+	box.innerHTML = ''
 	for (let i = 0; i < squares; i++) {
 		const square = document.createElement('div')
 		square.classList.add('square')
+		square.style.transitionDuration = speed
 
 		square.addEventListener('mouseover', () => setColor(square))
 		square.addEventListener('mouseout', () => removeColor(square))
-        
-        
+
 		box.appendChild(square)
 	}
 }
 
 const setColor = square => {
-    let h
-    
+	let h
+
 	if (range === 360) {
-        h = Math.floor(Math.random() * 360)
+		h = Math.floor(Math.random() * 360)
 	} else {
-        h = Math.floor(Math.random() * 60) + range
+		h = Math.floor(Math.random() * 60) + range
 	}
-    
+
 	const s = slider.value + '%'
 	const l = '50%'
-    
+
 	square.style.backgroundColor = `hsl(${h},${s},${l})`
 }
 
-const removeColor = (square) => {
-    
-    square.style.backgroundColor = `transparent`
-    
+const removeColor = square => {
+	square.style.backgroundColor = `transparent`
 }
 
-const handleSpeed = () => {
-    const newSpeed = this.dataset.speed + 's'
+function handleSpeed() {
+	const newSpeed = this.dataset.speed + 's'
+	createSquare(newSpeed)
 }
 
-createSquares()
+function handleColorRange() {
+	range = parseInt(this.dataset.colorRange)
+}
 
+const showSliderInfo = () => {
+    sliderInfo.textContent=slider.value
+}
+
+speedBtns.forEach(btn => btn.addEventListener('click', handleSpeed))
+colorBtns.forEach(btn => btn.addEventListener('click', handleColorRange))
+slider.addEventListener('mousemove', showSliderInfo)
+createSquare()
